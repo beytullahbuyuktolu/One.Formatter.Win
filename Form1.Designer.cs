e10bc4478fcf71c204622c9d93d8f2ec807ef0f6
beytullah.buyuktolu@teknosol.com.tr <beytullah.buyuktolu@teknosol.com.tr>
2025-03-06 12:16:25 +0300

    designer edit.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,10 +30,11 @@ namespace One.Formatter
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            
             txtInput = new TextBox();
             btnFormat = new Button();
             lblTitle = new Label();
-            lblInstructions = new Label();
             txtFileName = new TextBox();
             lblFileName = new Label();
             cboFormatType = new ComboBox();
@@ -43,162 +44,170 @@ namespace One.Formatter
             lblOutputTitle = new Label();
             btnSave = new Button();
             
+            SuspendLayout();
+            
             // 
             // txtInput
             // 
             txtInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left)));
-            txtInput.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            txtInput.Location = new System.Drawing.Point(12, 140);
+            txtInput.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            txtInput.Font = new System.Drawing.Font("Consolas", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            txtInput.Location = new System.Drawing.Point(12, 80);
             txtInput.Multiline = true;
             txtInput.Name = "txtInput";
             txtInput.ScrollBars = ScrollBars.Both;
-            txtInput.Size = new System.Drawing.Size(400, 312);
-            txtInput.TabIndex = 4;
+            txtInput.Size = new System.Drawing.Size(400, 350);
+            txtInput.TabIndex = 1;
             txtInput.WordWrap = false;
             txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
+            
             // 
             // txtOutput
             // 
             txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Right)));
-            txtOutput.BackColor = System.Drawing.Color.White;
-            txtOutput.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            txtOutput.Location = new System.Drawing.Point(428, 140);
+            txtOutput.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            txtOutput.Font = new System.Drawing.Font("Consolas", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            txtOutput.Location = new System.Drawing.Point(428, 80);
             txtOutput.Multiline = true;
             txtOutput.Name = "txtOutput";
             txtOutput.ReadOnly = true;
             txtOutput.ScrollBars = ScrollBars.Both;
-            txtOutput.Size = new System.Drawing.Size(400, 312);
-            txtOutput.TabIndex = 8;
+            txtOutput.Size = new System.Drawing.Size(400, 350);
+            txtOutput.TabIndex = 2;
             txtOutput.WordWrap = false;
+            
+            // 
+            // lblTitle
+            // 
+            lblTitle.AutoSize = true;
+            lblTitle.Font = new System.Drawing.Font("Segoe UI Semibold", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            lblTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(99)))), ((int)(((byte)(177)))));
+            lblTitle.Location = new System.Drawing.Point(12, 9);
+            lblTitle.Name = "lblTitle";
+            lblTitle.Size = new System.Drawing.Size(200, 25);
+            lblTitle.Text = "Multi-Format Parser";
+            
+            // 
+            // cboFormatType
+            // 
+            cboFormatType.DropDownStyle = ComboBoxStyle.DropDownList;
+            cboFormatType.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            cboFormatType.FormattingEnabled = true;
+            cboFormatType.Location = new System.Drawing.Point(678, 12);
+            cboFormatType.Name = "cboFormatType";
+            cboFormatType.Size = new System.Drawing.Size(150, 25);
+            cboFormatType.TabIndex = 0;
+            cboFormatType.SelectedIndexChanged += new System.EventHandler(this.cboFormatType_SelectedIndexChanged);
+            
+            // 
+            // lblFormatType
+            // 
+            lblFormatType.AutoSize = true;
+            lblFormatType.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            lblFormatType.Location = new System.Drawing.Point(598, 15);
+            lblFormatType.Name = "lblFormatType";
+            lblFormatType.Size = new System.Drawing.Size(74, 15);
+            lblFormatType.Text = "Format Type:";
+            
             // 
             // lblInputTitle
             // 
             lblInputTitle.AutoSize = true;
-            lblInputTitle.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            lblInputTitle.Location = new System.Drawing.Point(12, 120);
+            lblInputTitle.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            lblInputTitle.Location = new System.Drawing.Point(12, 60);
             lblInputTitle.Name = "lblInputTitle";
-            lblInputTitle.Size = new System.Drawing.Size(45, 20);
-            lblInputTitle.TabIndex = 9;
             lblInputTitle.Text = "Input";
+            
             // 
             // lblOutputTitle
             // 
             lblOutputTitle.AutoSize = true;
-            lblOutputTitle.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            lblOutputTitle.Location = new System.Drawing.Point(428, 120);
+            lblOutputTitle.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            lblOutputTitle.Location = new System.Drawing.Point(428, 60);
             lblOutputTitle.Name = "lblOutputTitle";
-            lblOutputTitle.Size = new System.Drawing.Size(152, 20);
-            lblOutputTitle.TabIndex = 10;
             lblOutputTitle.Text = "Formatted Output";
+            
             // 
             // btnFormat
             // 
             btnFormat.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            btnFormat.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            btnFormat.Location = new System.Drawing.Point(12, 467);
+            btnFormat.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            btnFormat.FlatAppearance.BorderSize = 0;
+            btnFormat.FlatStyle = FlatStyle.Flat;
+            btnFormat.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            btnFormat.ForeColor = System.Drawing.Color.White;
+            btnFormat.Location = new System.Drawing.Point(12, 445);
             btnFormat.Name = "btnFormat";
-            btnFormat.Size = new System.Drawing.Size(120, 40);
-            btnFormat.TabIndex = 5;
+            btnFormat.Size = new System.Drawing.Size(90, 30);
             btnFormat.Text = "Format";
-            btnFormat.UseVisualStyleBackColor = true;
+            btnFormat.UseVisualStyleBackColor = false;
             btnFormat.Click += new System.EventHandler(this.btnFormat_Click);
+            
             // 
             // btnSave
             // 
             btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            btnSave.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            btnSave.Location = new System.Drawing.Point(708, 467);
+            btnSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            btnSave.FlatAppearance.BorderSize = 0;
+            btnSave.FlatStyle = FlatStyle.Flat;
+            btnSave.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            btnSave.ForeColor = System.Drawing.Color.White;
+            btnSave.Location = new System.Drawing.Point(738, 445);
             btnSave.Name = "btnSave";
-            btnSave.Size = new System.Drawing.Size(120, 40);
-            btnSave.TabIndex = 11;
+            btnSave.Size = new System.Drawing.Size(90, 30);
             btnSave.Text = "Save";
-            btnSave.UseVisualStyleBackColor = true;
+            btnSave.UseVisualStyleBackColor = false;
             btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            
             // 
-            // lblTitle
-            // 
-            lblTitle.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            lblTitle.AutoSize = true;
-            lblTitle.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            lblTitle.Location = new System.Drawing.Point(301, 9);
-            lblTitle.Name = "lblTitle";
-            lblTitle.Size = new System.Drawing.Size(238, 32);
-            lblTitle.TabIndex = 2;
-            lblTitle.Text = "Multi-Format Parser";
-            // 
-            // lblInstructions
-            // 
-            lblInstructions.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            lblInstructions.AutoSize = true;
-            lblInstructions.Location = new System.Drawing.Point(301, 80);
-            lblInstructions.Name = "lblInstructions";
-            lblInstructions.Size = new System.Drawing.Size(413, 20);
-            lblInstructions.TabIndex = 3;
-            lblInstructions.Text = "Enter your text on the left and see formatted output on the right.";
-            //
             // lblFileName
-            //
+            // 
             lblFileName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             lblFileName.AutoSize = true;
-            lblFileName.Location = new System.Drawing.Point(428, 477);
+            lblFileName.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            lblFileName.Location = new System.Drawing.Point(428, 451);
             lblFileName.Name = "lblFileName";
-            lblFileName.Size = new System.Drawing.Size(77, 20);
-            lblFileName.TabIndex = 4;
+            lblFileName.Size = new System.Drawing.Size(71, 17);
             lblFileName.Text = "File Name:";
             lblFileName.Visible = false;
-            //
+            
+            // 
             // txtFileName
-            //
+            // 
             txtFileName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            txtFileName.Location = new System.Drawing.Point(505, 474);
+            txtFileName.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            txtFileName.Location = new System.Drawing.Point(505, 448);
             txtFileName.Name = "txtFileName";
-            txtFileName.Size = new System.Drawing.Size(197, 27);
-            txtFileName.TabIndex = 2;
+            txtFileName.Size = new System.Drawing.Size(220, 25);
+            txtFileName.TabIndex = 3;
             txtFileName.Visible = false;
-            //
-            // lblFormatType
-            //
-            lblFormatType.AutoSize = true;
-            lblFormatType.Location = new System.Drawing.Point(12, 50);
-            lblFormatType.Name = "lblFormatType";
-            lblFormatType.Size = new System.Drawing.Size(98, 20);
-            lblFormatType.TabIndex = 6;
-            lblFormatType.Text = "Format Type:";
-            //
-            // cboFormatType
-            //
-            cboFormatType.DropDownStyle = ComboBoxStyle.DropDownList;
-            cboFormatType.FormattingEnabled = true;
-            cboFormatType.Location = new System.Drawing.Point(116, 47);
-            cboFormatType.Name = "cboFormatType";
-            cboFormatType.Size = new System.Drawing.Size(200, 28);
-            cboFormatType.TabIndex = 1;
-            cboFormatType.SelectedIndexChanged += new System.EventHandler(this.cboFormatType_SelectedIndexChanged);
+            
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(840, 520);
-            this.Controls.Add(btnSave);
-            this.Controls.Add(lblOutputTitle);
-            this.Controls.Add(lblInputTitle);
-            this.Controls.Add(txtOutput);
-            this.Controls.Add(cboFormatType);
-            this.Controls.Add(lblFormatType);
-            this.Controls.Add(txtFileName);
-            this.Controls.Add(lblFileName);
-            this.Controls.Add(lblInstructions);
-            this.Controls.Add(lblTitle);
-            this.Controls.Add(btnFormat);
-            this.Controls.Add(txtInput);
-            this.MinimumSize = new System.Drawing.Size(858, 567);
-            this.Name = "Form1";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "One.Formatter - Multi-Format Parser";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            BackColor = System.Drawing.Color.White;
+            ClientSize = new System.Drawing.Size(840, 480);
+            Controls.Add(txtFileName);
+            Controls.Add(lblFileName);
+            Controls.Add(btnSave);
+            Controls.Add(btnFormat);
+            Controls.Add(lblOutputTitle);
+            Controls.Add(lblInputTitle);
+            Controls.Add(txtOutput);
+            Controls.Add(txtInput);
+            Controls.Add(cboFormatType);
+            Controls.Add(lblFormatType);
+            Controls.Add(lblTitle);
+            MinimumSize = new System.Drawing.Size(856, 519);
+            Name = "Form1";
+            StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            Text = "One.Formatter - Multi-Format Parser";
+            Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            Load += new System.EventHandler(this.Form1_Load);
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -206,7 +215,6 @@ namespace One.Formatter
         private TextBox txtInput;
         private Button btnFormat;
         private Label lblTitle;
-        private Label lblInstructions;
         private TextBox txtFileName;
         private Label lblFileName;
         private ComboBox cboFormatType;
